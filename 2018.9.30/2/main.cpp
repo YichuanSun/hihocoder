2e62983a1deb0432db9c48b719a5e8e6573b8fd5
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-01 11:54:07 +0800

    还在想为啥不行，原来题意忘了。。。

--- 2018.9.30/2/main.cpp
+++ 2018.9.30/2/main.cpp
@@ -1,33 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<pair<int,int> > vii;
-bool flag1;
-void read_input();
+int a[7][3];
+bool f,used[10];
+void dfs(int l) {
+    if (f)  return;
+    if (l==7)   {
+        if (a[1][1]==a[2][2]&&a[2][2]==a[5][1])
+            if (a[1][2]==a[3][1]&&a[3][1]==a[5][2])
+                if (a[2][1]==a[4][2])
+                    if (a[3][2]==a[6][1])
+                        f=true;
+        return;
+    }
+    for (int i=1;i<=6;i++)  {
+        if (used[i])    continue;
+        used[i]=true;
+        dfs(l+1);
+        if (f)  return;
+        swap(a[i][1],a[i][2]);
+        dfs(l+1);
+        used[i]=false;
+    }
+//    for (int i=l;i<=6;i++)  {
+//        dfs(l+1);
+//        if (f)  return;
+//        swap(a[i][1],a[i][2]);
+//        dfs(l+1);
+//        if (f)  return;
+//    }
+}
 int main()  {
     int n;
-    while (n--) {
-        read_input();
-        if (flag1)  {
-            printf("NO\n");
-//            continue;
-        }
-        else    {
-
-        }
-        flag1=false;
-        vii.clear();
+    scanf("%d",&n);
+    for (int i=0;i<n;i++)   {
+        for (int i=1;i<=6;i++)
+            scanf("%d%d",&a[i][1],&a[i][2]);
+        dfs(1);
+        if (f)  printf("YES\n");
+        else printf("NO\n");
+        f=false;
+        fill(used,used+10,false);
     }
     return 0;
 }
-
-void read_input()   {
-    pair<int,int> p;
-    for (int i=1;i<=6;i++)  {
-        scanf("%d%d",p.first,p.second);
-        if (abs(p.first-p.second)>1)    flag1=true;
-        vii.push_back(p);
-    }
-}
-
-
-
