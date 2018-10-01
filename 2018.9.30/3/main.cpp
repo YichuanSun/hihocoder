f62d24d789f7727583a851028d59a44adcbef9bd
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-01 13:18:37 +0800

    这两道题是你没实力做吗？不是吧，其实我可以做出来，就差一点点

--- 2018.9.30/3/main.cpp
+++ 2018.9.30/3/main.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
+typedef long long ll;
+int a[100],cnt;
+int main()  {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll n;
+    cin>>n;
+    if (n==1)   {cout<<1<<endl;return 0;}
+    for (int i=9;i>=2;i--)  {
+        while (n%i==0)  {
+            n/=i;
+            a[cnt++]=i;
+        }
+    }
+    if (cnt==0) cout<<-1;
+    else for (int i=cnt-1;i>=0;i--) cout<<a[i];
+    cout<<endl;
     return 0;
 }
