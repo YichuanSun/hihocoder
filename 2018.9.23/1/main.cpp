b3ed439202c3a02c4f0a257592c30f08cdaf3c03
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-03 19:58:05 +0800

    嗯？为何如此简单的算法，我当时却没想到？

--- 2018.9.23/1/main.cpp
+++ 2018.9.23/1/main.cpp
@@ -1,35 +1,22 @@
-#include <bits.stdc++.h>
-#define N 200005
+#include <bits/stdc++.h>
+#define N 100005
 using namespace std;
-struct node{
-    int a,b;
-    node(int a1,int b1){a=a1;b=b1;}
-};
-node nds[N];
-int cmp(node A,node B)  {
-    if (A.a==B.a)   return A.b>B.b;
-    return A.a>B.a;
+int x[N],y[N],lst[N];
+bool cmp(int a,int b)   {
+    return x[a]>x[b];
 }
 int main()  {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n,asum=0,bsum=0,asp=0,bsp=0;
-    cin>>n;
-    for (int i=0;i<2*n+1;i++)  {
-        cin>>ta>>tb;
-        nds[i]=node(ta,tb);
+    int n;
+    scanf("%d",&n);
+    for (int i=1;i<=2*n+1;i++)  {
+        scanf("%d%d",&x[i],&y[i]);
+        lst[i]=i;
     }
-    sort(nds,nds+2*n+1,cmp);
-    for (int i=0;i<=n;i++)  {
-        asum+=nds[i].a;
-        bsum+=nds[i].b;
-    }
-    for (int i=n+1;i<2*n+1;i++) {
-        asp+=nds[i].a;
-        bsp+=nds[i].b;
+    sort(lst+1,lst+2*n+2,cmp);
+    printf("%d\n",lst[1]);
+    for (int i=2;i<2*(n+1);i+=2)    {
+        if (y[lst[i]]>y[lst[i+1]])    printf("%d\n",lst[i]);
+        else    printf("%d\n",lst[i+1]);
     }
     return 0;
 }
-
-
-
